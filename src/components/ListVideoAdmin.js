c18d897190a4ffd1d3ac52413d5f4f80b9c632b7
Aditya Pratama Badra <orochimadit@gmail.com>
2021-08-31 15:25:06 +0700

    delete video done

--- src/components/ListVideoAdmin.js
+++ src/components/ListVideoAdmin.js
@@ -1,13 +1,27 @@
 import { useEffect,useState } from "react";
 import {Modal, Button} from 'react-bootstrap';
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react-dom";
 import ReactPlayer from "react-player/lazy";
+import swal from "sweetalert";
 const ListVideoAdmin = () =>{
      const [dataListVideo, setDataListVideo] = useState([]);
      const [handleShowVideo, setHandleShowVideo] = useState(false);
      const [linkVideo,setLinkVideo]= useState('');
-
-     const handleClose = () =>{
+     const [lgShow, setLgShow] = useState(false);
+     const [judul, setJudul] = useState('');
+     const [keterangan, setKeterangan] = useState('');
+     const [linkThumbnail, setThumbnail] =useState('');
+     const [linkVideoUpload, setLinkVideoUpload] = useState('');
+    const [show,setShowDelete ] = useState(false);
+    const [idDel, setIdDel ]= useState('');
+    const handleClose = () =>{
          setHandleShowVideo(false)
+        setShowDelete(false)
+    }
+
+     const handleShow = (id) =>{
+        setShowDelete(true)
+        setIdDel(id)
      }
     useEffect(() =>{
         getData()
@@ -38,6 +52,74 @@ const ListVideoAdmin = () =>{
         setHandleShowVideo(true)
         console.log(data)
         setLinkVideo(data.link_video)
+    }
+    const handleSimpan = (e) =>{
+        e.preventDefault();
+        const token = localStorage.getItem('dataLoginAdmin');
+        const dataSend ={
+            judul : judul,
+            keterangan : keterangan,
+            link_thumbnail: linkThumbnail,
+            link_video : linkVideoUpload,
+            token : token
+        }
+        if( judul ===''||
+            keterangan === ''||
+            linkVideoUpload ==='' ||
+            linkThumbnail === ''){
+                swal('Gagal','Form Harus terisi Semua!','error');
+                return;
+            }
+        fetch(`${process.env.REACT_APP_API}/tambahKonten`,{
+            method:"POST",
+            body : JSON.stringify(dataSend),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(hasil => {
+            clearState();
+            console.log('hasil => ',hasil);
+            setLgShow(false);
+            if(hasil.status ==='berhasil'){
+                swal("success",hasil.message,"success")
+                getData()
+            }else{
+                swal("failed",hasil.message,"error")
+            }
+            
+        })
+    }
+
+    const clearState = () =>{
+        setJudul('')
+        setKeterangan('');
+        setThumbnail('');
+        setLinkVideoUpload('')
+    }
+    const handleDelete = ()=>{
+        const token = localStorage.getItem('dataLoginAdmin')
+        const dataSend ={
+            id_konten : idDel,
+            token: token
+        }
+        fetch(`${process.env.REACT_APP_API}/hapusKonten`,{
+            method: 'POST',
+            body: JSON.stringify(dataSend),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res=> res.json())
+        .then(hasil =>{
+            getData();
+            setShowDelete(false);
+            swal("success",hasil.message,"success")
+        })
+        .catch(err=>{
+            alert(err)
+        })
     }
     return(
     <>
@@ -81,12 +163,90 @@ const ListVideoAdmin = () =>{
           <Button variant="primary">Understood</Button>
         </Modal.Footer>
       </Modal>
+      {/* Modal tambah video */}
+      <Modal
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Large Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="judul">Judul</label>
+                    <input
+                        onChange ={(e) =>setJudul(e.target.value)}
+                        value={judul}
+                        type="text"
+                        className="form-control"
+                        id="judul"
+                        placeholder="judul"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="keterangan">Keterangan</label>
+                    <input
+                        onChange ={(e) =>setKeterangan(e.target.value)}
+                        value={keterangan}
+                        type="text"
+                        className="form-control"
+                        id="keterangan"
+                        placeholder="keterangan"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="link_thumbnail">link thumbnail</label>
+                    <input
+                       onChange ={(e) =>setThumbnail(e.target.value)}
+                       value={linkThumbnail}
+                        type="text"
+                        className="form-control"
+                        id="link_thumbnail"
+                        placeholder="link_thumbnail"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="link_video">Link Video</label>
+                    <input
+                        onChange ={(e) =>setLinkVideoUpload(e.target.value)}
+                        value={linkVideoUpload}
+                        type="text"
+                        className="form-control"
+                        id="link_video"
+                        placeholder="link_video"
+                    />
+                </div>
+                <button onClick={(e) => handleSimpan(e)} className="btn btn-primary">Simpan</button>
+            </form>
+        </Modal.Body>
+      </Modal>
+        {/* modal delete video */}
+
+        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
         <div className="jumbotron">
             <h1 className="display-4">Hello, world!</h1>
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             {/* <hr className="my-4"> */}
             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <button onClick={()=> setLgShow(true)} className="btn btn-primary btn-lg" href="#" role="button">Tambah Video</button>
         </div>
 
         <div className="row justify-content-center">
@@ -101,7 +261,7 @@ const ListVideoAdmin = () =>{
                 <div className="card-body">
                     <h5 className="card-title">{data.judul}</h5>
                     <p className="card-text">{data.keterangan}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <button onClick={()=> handleShow(data.id_konten)} href="#" className="btn btn-danger mr-3">Go somewhere</button>
                 </div>
             </div>
                 )
